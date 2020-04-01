984e9919267069f87eb6625b13c69295cdc3a6e6
Chi Igbokwe <60106736+devnchi@users.noreply.github.com>
2020-04-01 15:10:36 -0400

    Expressions Matter (8kyu)

---
+++ expressionsMatter.js
@@ -0,0 +1,7 @@
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ().
+
+function expressionMatter(a, b, c) {
+  return Math.max(a+b+c, (a+b)*c, a*(b+c), a*b*c)
+}
+
+expressionMatter(5, 6, 9);
